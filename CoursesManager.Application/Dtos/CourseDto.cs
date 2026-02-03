6f7e6accd236cf57334645b11791bb6416746505
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-03 15:07:13 +0100

    add CourseDto to Application

---
+++ CoursesManager.Application/Dtos/CourseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursesManager.Application.Dtos;
+
+internal class CourseDto
+{
+    public string? CourseCode { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
