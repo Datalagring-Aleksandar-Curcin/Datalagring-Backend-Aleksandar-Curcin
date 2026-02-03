ce77e4acbe66ab2346e21246b4cd0906e56525ea
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-03 14:58:18 +0100

    add Abstraction/Persistance folders with IBaseRepository interface

---
+++ CoursesManager.Application/Abstractions/Persistence/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace CoursesManager.Application.Abstractions.Persistence
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> CreateAsync(TEntity entity, CancellationToken ct = default);
+        Task<bool> ExistAsync(Expression<Func<TEntity, bool>> findBy);
+
+        Task<IReadOnlyList<TEntity>> GetAllAsync(
+        Expression<Func<TEntity, bool>>? where = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        bool tracking = false,
+        CancellationToken ct = default,
+        params Expression<Func<TEntity, object>>[] includes);
+
+        Task<IReadOnlyList<TSelect>> GetAllAsync<TSelect>(
+        Expression<Func<TEntity, TSelect>> select,
+        Expression<Func<TEntity, bool>>? where = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        bool tracking = false,
+        CancellationToken ct = default,
+        params Expression<Func<TEntity, object>>[] includes);
+    }
+}
