3cc3b5390141cc2d228f0029be6d77242042652c
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-12 16:47:38 +0100

    add Created result type

--- CoursesManager.Application/Common/Results/Result.cs
+++ CoursesManager.Application/Common/Results/Result.cs
@@ -1,8 +1,10 @@
 ﻿namespace CoursesManager.Application.Common.Results;
 
 public readonly record struct Deleted;
+public readonly record struct Created;
 
 public static class Result
 {
     public static Deleted Deleted => default;
+    public static Created Created => default;
 }
