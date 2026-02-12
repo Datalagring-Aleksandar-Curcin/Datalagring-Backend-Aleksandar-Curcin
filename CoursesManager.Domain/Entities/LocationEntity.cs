96a4ce85aba5fef13707d666063e042ccb26035c
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-12 17:17:45 +0100

    add LocationEntity with CourseSessionEntity relation to Domain

---
+++ CoursesManager.Domain/Entities/LocationEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CoursesManager.Domain.Entities;
+
+internal class LocationEntity
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public ICollection<CourseSessionEntity> CourseSessions { get; set; } = [];
+}
