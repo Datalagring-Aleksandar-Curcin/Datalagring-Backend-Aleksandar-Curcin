42d97b2189f745f13ed247d2f34ac02e7b5580f5
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-12 17:11:44 +0100

    add CourseSessionService get-by-id

---
+++ CoursesManager.Application/Services/CourseSessionService.cs
@@ -0,0 +1,27 @@
+﻿using CoursesManager.Application.Abstractions.Persistence;
+using CoursesManager.Application.Common.Errors;
+using CoursesManager.Application.Dtos.CourseSessions;
+using CoursesManager.Application.Mappers;
+
+namespace CoursesManager.Application.Services;
+
+public class CourseSessionService(ICourseSessionRepository repo)
+{
+
+    
+    public async Task<ErrorOr<CourseSessionDto>> GetOneCourseSessionAsync(int id, CancellationToken ct = default)
+    {
+        var courseSession = await repo.GetOneAsync(
+            where: cs => cs.Id == id,
+            select: CourseSessionMapper.ToCourseSessionDtoExpr,
+            ct: ct
+        );
+
+        return courseSession is null
+            ? Error.NotFound("CourseSession.NotFound", $"CourseSession with '{id}' was not found.")
+            : courseSession;
+    }
+}
+
+
+
