3888541327e0636e896a2bc5bd524d55319df9cf
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-12 17:06:30 +0100

    add CourseSessions navigation to courseEntity

--- CoursesManager.Domain/Entities/CourseEntity.cs
+++ CoursesManager.Domain/Entities/CourseEntity.cs
@@ -9,6 +9,8 @@ public class CourseEntity
     public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
     public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
     public byte[] RowVersion { get; set; } = [];
+
+    public ICollection<CourseSessionEntity> CourseSessions { get; set; } = [];
 }
