30249ee17188888fe567bcc3d757a47d89f21125
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-12 15:33:06 +0100

    add course creation service with duplicate check

--- CoursesManager.Application/Services/CourseService.cs
+++ CoursesManager.Application/Services/CourseService.cs
@@ -1,8 +1,24 @@
-﻿namespace CoursesManager.Application.Services;
+﻿using CoursesManager.Application.Abstractions.Persistence;
+using CoursesManager.Application.Common.Errors;
+using CoursesManager.Application.Dtos;
+using CoursesManager.Application.Mappers;
+using CoursesManager.Domain.Entities;
+
+namespace CoursesManager.Application.Services;
 
 public class CourseService(ICourseRepository courseRepository)
 {
 
-    private readonly ICourseRepository
+    private readonly ICourseRepository _courseRepository = courseRepository;
+
+    public async Task<ErrorOr<CourseDto>> CreateCourseAsync(CreateCourseDto dto, CancellationToken ct = default)
+    {
+        var exist = await _courseRepository.ExistAsync(x => x.CourseCode == dto.CourseCode);
+        if (exist)
+            return Error.Conflict("Course.Conflict", $"Course with '{dto.CourseCode}' already exists.");
+
+        var savedCourse = await _courseRepository.CreateAsync(new CourseEntity { CourseCode = dto.CourseCode, Title = dto.Title, Description = dto.Description}, ct);
+        return CourseMapper.ToCourseDto(savedCourse);
+    }
 
 }
