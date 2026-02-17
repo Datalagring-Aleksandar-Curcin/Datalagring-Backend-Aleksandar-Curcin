c3861fbb40291752570ef5f1e37feb8a6b8e97a0
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 14:30:26 +0100

    add CourseSessionConfig to infrastructure and set PK for coursesession Id

---
+++ CoursesManager.Infrastructure/Persistence/Configurations/CourseSessionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using CoursesManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoursesManager.Infrastructure.Persistence.Configurations;
+
+public class CourseSessionConfiguration : IEntityTypeConfiguration<CourseSessionEntity>
+{
+    public void Configure(EntityTypeBuilder<CourseSessionEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+    }
+}
