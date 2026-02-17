c89c4aa653e0b64d2fdb3e5f55cb7de15d00bd38
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 18:45:26 +0100

    implement InstanceTeacherConfig to infrastructure

---
+++ CoursesManager.Infrastructure/Persistence/Configurations/InstanceTeacherConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CoursesManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoursesManager.Infrastructure.Persistence.Configurations;
+
+internal class InstanceTeacherConfiguration : IEntityTypeConfiguration<InstanceTeacherEntity>
+{
+    public void Configure(EntityTypeBuilder<InstanceTeacherEntity> builder)
+    {
+        builder.ToTable("InstanceTeachers");
+
+        builder.HasKey(x => new { x.CourseSessionId, x.TeacherId });
+
+        builder.HasOne(x => x.CourseSession)
+            .WithMany(cs => cs.InstanceTeachers)
+            .HasForeignKey(x => x.CourseSessionId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.Teacher)
+            .WithMany(t => t.InstanceTeachers)
+            .HasForeignKey(x => x.TeacherId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+    }
+}
