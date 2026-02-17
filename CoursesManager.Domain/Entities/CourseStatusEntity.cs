2942f556d29a940f1c11730e4a36c3ac4c59df1b
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 18:14:53 +0100

    implement CourseStatusEntity to Domain

---
+++ CoursesManager.Domain/Entities/CourseStatusEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CoursesManager.Domain.Entities;
+
+public class CourseStatusEntity
+{
+    public int Id { get; set; }
+    public string StatusType { get; set; } = null!;
+
+    public ICollection<CourseRegistrationEntity> CourseRegistrations { get; set; } = [];
+}
