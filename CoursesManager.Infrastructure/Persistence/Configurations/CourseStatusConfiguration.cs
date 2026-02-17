5e3417ecb7d515fda5c7926e88c22729a8bcb941
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 19:37:27 +0100

    implement coursestatusConfig to infrastructure

---
+++ CoursesManager.Infrastructure/Persistence/Configurations/CourseStatusConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CoursesManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoursesManager.Infrastructure.Persistence.Configurations;
+
+public class CourseStatusConfiguration : IEntityTypeConfiguration<CourseStatusEntity>
+{
+    public void Configure(EntityTypeBuilder<CourseStatusEntity> builder)
+    {
+        builder.ToTable("CourseStatus");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.StatusType)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasIndex(x => x.StatusType).IsUnique();
+    }
+}
