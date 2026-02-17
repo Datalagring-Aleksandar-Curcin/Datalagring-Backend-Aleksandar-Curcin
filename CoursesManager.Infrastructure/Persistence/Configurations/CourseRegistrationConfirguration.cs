9b343e50756cc85b76acafc881b23e5651487b70
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 19:39:42 +0100

    implement CourseRegistrationConfig to infrastructure

---
+++ CoursesManager.Infrastructure/Persistence/Configurations/CourseRegistrationConfirguration.cs
@@ -0,0 +1,39 @@
+﻿using CoursesManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoursesManager.Infrastructure.Persistence.Configurations;
+
+internal class CourseRegistrationConfirguration : IEntityTypeConfiguration<CourseRegistrationEntity>
+{
+    public void Configure(EntityTypeBuilder<CourseRegistrationEntity> builder)
+    {
+        builder.ToTable("CourseRegistrations");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.RegistrationDate)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSUTCDATETIME()");
+
+        builder.Property(x => x.UpdatedAt)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSUTCDATETIME()");
+
+
+        builder.HasOne(x => x.Participant)
+            .WithMany(p => p.CourseRegistrations)
+            .HasForeignKey(x => x.ParticipantId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.CourseSession)
+            .WithMany(cs => cs.CourseRegistrations)
+            .HasForeignKey(x => x.CourseSessionId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.CourseStatus)
+            .WithMany(s => s.CourseRegistrations)
+            .HasForeignKey(x => x.CourseStatusId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
