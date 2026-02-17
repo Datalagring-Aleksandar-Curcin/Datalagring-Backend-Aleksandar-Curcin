969a53a61e359ce8283f45a6e6f4862473ae411c
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 13:21:21 +0100

    Add global exception handler using ProblemDetails

---
+++ CoursesManager.Presentation/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursesManager.Presentation.Middlewares
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            //logger
+
+            httpContext.Response.StatusCode = exception switch
+            {
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            return await httpContext.RequestServices
+                .GetRequiredService<IProblemDetailsService>()
+                .TryWriteAsync(new ProblemDetailsContext
+                {
+                    HttpContext = httpContext,
+                    ProblemDetails = new ProblemDetails
+                    {
+                        Title = "Server Error",
+                        Detail = "An unexpected error occured. Please try again"
+                    }
+                });
+        }
+    }
+}
