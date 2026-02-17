9ecc51469e29afcf80bef7abd7d9cd7e591604af
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 18:12:33 +0100

    implement ParticipantEntity to Domain

---
+++ CoursesManager.Domain/Entities/ParticipantEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace CoursesManager.Domain.Entities;
+
+public class ParticipantEntity
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public ICollection<CourseRegistrationEntity> CourseRegistrations { get; set; } = [];
+}
