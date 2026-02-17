14b93d3627415ae5d6660ab63ccef1b831cdcdb7
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 18:33:58 +0100

    implement Teacher config to infrastructure

---
+++ CoursesManager.Infrastructure/Persistence/Configurations/TeacherConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using CoursesManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoursesManager.Infrastructure.Persistence.Configurations;
+
+public class TeacherConfiguration : IEntityTypeConfiguration<TeacherEntity>
+{
+    public void Configure(EntityTypeBuilder<TeacherEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Expertise)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSUTCDATETIME()");
+
+        builder.Property(x => x.UpdatedAt)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSUTCDATETIME()");
+
+    }
+}
