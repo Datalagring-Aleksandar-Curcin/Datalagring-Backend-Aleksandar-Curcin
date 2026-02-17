ce0a1dc6909d292eb8c24709273ea98f2e23dd74
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 18:08:54 +0100

    implement InstanceTeacherEntity to Domain

---
+++ CoursesManager.Domain/Entities/InstanceTeacherEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursesManager.Domain.Entities;
+
+public class InstanceTeacherEntity
+{
+    public int CourseSessionId { get; set; }
+    public CourseSessionEntity CourseSession { get; set; } = null!;
+
+    public int TeacherId { get; set; }
+    public TeacherEntity Teacher { get; set; } = null!;
+}
