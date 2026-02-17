220fff0f130bc41fab6128cc67c27027005ae91b
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 19:31:05 +0100

    implement ParticipantConfig to infrastructure

---
+++ CoursesManager.Infrastructure/Persistence/Configurations/ParticipantConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using CoursesManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoursesManager.Infrastructure.Persistence.Configurations;
+
+internal class ParticipantConfiguration : IEntityTypeConfiguration<ParticipantEntity>
+{
+    public void Configure(EntityTypeBuilder<ParticipantEntity> builder)
+    {
+        builder.ToTable("Participants");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasColumnType("varchar(255)");
+
+        builder.HasIndex(x => x.Email).IsUnique();
+
+        builder.Property(x => x.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(x => x.PhoneNumber)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSUTCDATETIME()");
+
+        builder.Property(x => x.UpdatedAt)
+            .HasColumnType("datetime2(0)")
+            .HasDefaultValueSql("SYSUTCDATETIME()");
+    }
+}
