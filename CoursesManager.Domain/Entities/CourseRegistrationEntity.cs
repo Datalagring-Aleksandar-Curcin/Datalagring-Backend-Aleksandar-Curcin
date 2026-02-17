19c1634639481ebf836b138a565a24766d0962ba
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-17 18:18:07 +0100

    implement CourseRegistrationEntity to Domain

---
+++ CoursesManager.Domain/Entities/CourseRegistrationEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace CoursesManager.Domain.Entities;
+
+public class CourseRegistrationEntity
+{
+    public int Id { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public bool IsCanceled { get; set; }
+
+
+    public int CourseSessionId { get; set; }
+    public CourseSessionEntity CourseSession { get; set; } = null!;
+
+
+    public int ParticipantId { get; set; }
+    public ParticipantEntity Participant { get; set; } = null!;
+
+
+    public int CourseStatusId { get; set; }
+    public CourseStatusEntity CourseStatus { get; set; } = null!;
+}
