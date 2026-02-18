ee7a43ddf345815c70e6305ac34de95014421ba3
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-18 11:14:32 +0100

    add final entities to ApplicationDbContext

--- CoursesManager.Infrastructure/Data/ApplicationDbContext.cs
+++ CoursesManager.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,8 +6,17 @@ namespace CoursesManager.Infrastructure.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<CourseEntity> Courses { get; set; }
+    public DbSet<CourseRegistrationEntity> CourseRegistrations { get; set; }
     public DbSet<CourseSessionEntity> CourseSessions { get; set; }
+    public DbSet<CourseStatusEntity> CourseStatus { get; set; }
+    public DbSet<InstanceTeacherEntity> InstanceTeachers { get; set; }
     public DbSet<LocationEntity> Locations { get; set; }
+    public DbSet<ParticipantEntity> Participants { get; set; }
+    public DbSet<TeacherEntity> Teachers { get; set; }
+
+
+
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
