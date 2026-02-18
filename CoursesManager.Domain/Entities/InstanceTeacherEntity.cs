89bcb8abf8b0033e29b46c5d51eb92f9728aa6f1
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-18 10:51:33 +0100

    change coursesession -> coursesessions

--- CoursesManager.Domain/Entities/InstanceTeacherEntity.cs
+++ CoursesManager.Domain/Entities/InstanceTeacherEntity.cs
@@ -3,7 +3,7 @@
 public class InstanceTeacherEntity
 {
     public int CourseSessionId { get; set; }
-    public CourseSessionEntity CourseSession { get; set; } = null!;
+    public CourseSessionEntity CourseSessions { get; set; } = null!;
 
     public int TeacherId { get; set; }
     public TeacherEntity Teacher { get; set; } = null!;
