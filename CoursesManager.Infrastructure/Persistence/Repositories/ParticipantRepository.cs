1343b555a0188134459b41f09e7c92de3e76c64b
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 08:30:46 +0100

    add ParticipantRepo to Infrastructure

---
+++ CoursesManager.Infrastructure/Persistence/Repositories/ParticipantRepository.cs
@@ -0,0 +1,9 @@
+﻿using CoursesManager.Application.Abstractions.Persistence;
+using CoursesManager.Domain.Entities;
+using CoursesManager.Infrastructure.Data;
+
+namespace CoursesManager.Infrastructure.Persistence.Repositories;
+
+public class ParticipantRepository(ApplicationDbContext context) : BaseRepository<ParticipantEntity>(context), IParticipantRepository
+{
+}
