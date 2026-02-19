a621a8f58a6cc8c0d50867011081bd10ce07bc1a
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 10:39:49 +0100

    add LocationMapper to Application

---
+++ CoursesManager.Application/Mappers/LocationMapper.cs
@@ -0,0 +1,24 @@
+﻿using CoursesManager.Application.Dtos.Locations;
+using CoursesManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace CoursesManager.Application.Mappers;
+
+public static class LocationMapper
+{
+    public static Expression<Func<LocationEntity, LocationDto>> ToLocationDtoExpr =>
+        l => new LocationDto(
+            l.Id, 
+            l.Name, 
+            l.CreatedAt, 
+            l.UpdatedAt
+        );
+
+    public static LocationDto ToLocationDto(LocationEntity entity) =>
+        new(
+            entity.Id, 
+            entity.Name, 
+            entity.CreatedAt, 
+            entity.UpdatedAt
+        );
+}
