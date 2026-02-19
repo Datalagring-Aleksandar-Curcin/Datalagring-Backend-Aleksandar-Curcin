539fe057c3d22ea6463af8ea42eaa4b5233f70ae
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 16:53:10 +0100

    add CourseStatusMapper to Application

---
+++ CoursesManager.Application/Mappers/CourseStatusMapper.cs
@@ -0,0 +1,17 @@
+﻿using CoursesManager.Application.Dtos.CourseStatus;
+using CoursesManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace CoursesManager.Application.Mappers;
+
+public static class CourseStatusMapper
+{
+    public static Expression<Func<CourseStatusEntity, CourseStatusDto>> ToCourseStatusDtoExpr =>
+        s => new CourseStatusDto(
+            s.Id,
+            s.StatusType
+        );
+
+    public static CourseStatusDto ToCourseStatusDto(CourseStatusEntity entity) =>
+        new(entity.Id, entity.StatusType);
+}
