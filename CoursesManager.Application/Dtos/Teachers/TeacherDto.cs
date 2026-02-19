585eb2f6329ce5388bf9a118387f1dd1ca81d2ab
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 15:21:02 +0100

    add TeacherDto to Application

---
+++ CoursesManager.Application/Dtos/Teachers/TeacherDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursesManager.Application.Dtos.Teachers;
+
+public record TeacherDto(
+    int Id,
+    string FirstName,
+    string LastName,
+    string Expertise,
+    DateTime CreatedAt,
+    DateTime UpdatedAt
+);
