c2ee30fec9872189c31b0b08b77851ba9486f52d
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 08:20:29 +0100

    add IParticipantRepo to Application

---
+++ CoursesManager.Application/Abstractions/Persistence/IParticipantRepository.cs
@@ -0,0 +1,7 @@
+﻿using CoursesManager.Domain.Entities;
+
+namespace CoursesManager.Application.Abstractions.Persistence;
+
+public interface IParticipantRepository : IBaseRepository<ParticipantEntity>
+{
+}
