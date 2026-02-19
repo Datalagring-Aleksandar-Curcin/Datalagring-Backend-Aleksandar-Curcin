02a498cbe65307b57394764245b36938881ce5cf
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 16:09:53 +0100

    add ParticipantDto to Application

---
+++ CoursesManager.Application/Dtos/Participants/ParticipantDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CoursesManager.Application.Dtos.Participants;
+
+public record ParticipantDto(
+    int Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string PhoneNumber,
+    DateTime CreatedAt,
+    DateTime UpdatedAt
+    
+);
---
+++ CoursesManager.Application/Dtos/Participants/UpdateParticipantDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoursesManager.Application.Dtos.Participants;
+
+public class UpdateParticipantDto
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(255)]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(50)]
+    public string PhoneNumber { get; set; } = null!;
+}
