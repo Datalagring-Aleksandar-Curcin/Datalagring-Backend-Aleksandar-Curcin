d5646cd6d8fe1560ee079615796c001690f25bbc
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 15:28:34 +0100

    add TeacherMapper to application

---
+++ CoursesManager.Application/Mappers/TeacherMapper.cs
@@ -0,0 +1,31 @@
+﻿using CoursesManager.Application.Dtos.Teachers;
+using CoursesManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace CoursesManager.Application.Mappers;
+
+public static class TeacherMapper
+{
+
+    public static TeacherDto ToTeacherDto(TeacherEntity entity) =>
+    new(
+        entity.Id, 
+        entity.FirstName, 
+        entity.LastName, 
+        entity.Expertise, 
+        entity.CreatedAt, 
+        entity.UpdatedAt
+    );
+
+    public static Expression<Func<TeacherEntity, TeacherDto>> ToTeacherDtoExpr =>
+        t => new TeacherDto(
+            t.Id,
+            t.FirstName,
+            t.LastName,
+            t.Expertise,
+            t.CreatedAt,
+            t.UpdatedAt
+        );
+
+
+}
