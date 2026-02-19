58663abf917cdcb7b83878c93082a5f8cd5029a6
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 10:32:25 +0100

    add CreateLocationDto to Application

---
+++ CoursesManager.Application/Dtos/Locations/CreateLocationDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoursesManager.Application.Dtos.Locations;
+
+public class CreateLocationDto
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
+}
