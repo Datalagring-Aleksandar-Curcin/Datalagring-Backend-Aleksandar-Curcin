e8d23a707020a1eb334b1c2a3dd25d258b598ec7
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 17:38:13 +0100

    add InstanceTeacherRepo to infrastructure

---
+++ CoursesManager.Infrastructure/Persistence/Repositories/InstanceTeacherRepository.cs
@@ -0,0 +1,10 @@
+﻿using CoursesManager.Application.Abstractions.Persistence;
+using CoursesManager.Domain.Entities;
+using CoursesManager.Infrastructure.Data;
+
+namespace CoursesManager.Infrastructure.Persistence.Repositories
+{
+    public class InstanceTeacherRepository(ApplicationDbContext context) : BaseRepository<InstanceTeacherEntity>(context), IInstanceTeacherRepository
+    {
+    }
+}
