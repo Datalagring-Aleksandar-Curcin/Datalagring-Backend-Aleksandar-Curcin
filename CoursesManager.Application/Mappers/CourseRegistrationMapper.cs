d50ec36ac48430227dd04b7e5a4a5e4f42d548ff
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 17:32:08 +0100

    add CourseRegistrationMapper to application

---
+++ CoursesManager.Application/Mappers/CourseRegistrationMapper.cs
@@ -0,0 +1,28 @@
+﻿using CoursesManager.Application.Dtos.CourseRegistrations;
+using CoursesManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace CoursesManager.Application.Mappers;
+
+public static class CourseRegistrationMapper
+{
+    public static Expression<Func<CourseRegistrationEntity, CourseRegistrationDto>> ToCourseRegistrationDtoExpr =>
+        r => new CourseRegistrationDto(
+            r.Id,
+            r.RegistrationDate,
+            r.UpdatedAt,
+            r.CourseSessionId,
+            r.ParticipantId,
+            r.CourseStatusId
+        );
+
+    public static CourseRegistrationDto ToCourseRegistrationDto(CourseRegistrationEntity entity) =>
+        new(
+            entity.Id, 
+            entity.RegistrationDate, 
+            entity.UpdatedAt, 
+            entity.CourseSessionId, 
+            entity.ParticipantId, 
+            entity.CourseStatusId
+        );
+}
