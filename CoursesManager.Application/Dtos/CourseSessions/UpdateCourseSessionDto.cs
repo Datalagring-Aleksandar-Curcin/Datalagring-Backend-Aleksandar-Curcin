3a0076f6beb82d9f7b021962f639f7c1bb3f4ca3
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 09:56:53 +0100

    add UpdateCourseSessionDto to Application

---
+++ CoursesManager.Application/Dtos/CourseSessions/UpdateCourseSessionDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoursesManager.Application.Dtos.CourseSessions;
+
+public record UpdateCourseSessionDto
+{
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int CourseId { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int LocationId { get; set; }
+
+    [Required]
+    public DateTime StartDate { get; set; }
+
+    [Required]
+    public DateTime EndDate { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int MaxParticipants { get; set; }
+}
