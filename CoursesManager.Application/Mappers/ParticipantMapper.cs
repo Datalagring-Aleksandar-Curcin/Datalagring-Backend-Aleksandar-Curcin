cb71119ad1bfec3ad77436a6abdc3a2c01ab700e
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 16:11:19 +0100

    add ParticipantMapper to Application

---
+++ CoursesManager.Application/Mappers/ParticipantMapper.cs
@@ -0,0 +1,30 @@
+﻿using CoursesManager.Application.Dtos.Participants;
+using CoursesManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace CoursesManager.Application.Mappers;
+
+public static class ParticipantMapper
+{
+    public static Expression<Func<ParticipantEntity, ParticipantDto>> ToParticipantDtoExpr =>
+        p => new ParticipantDto(
+            p.Id,
+            p.FirstName,
+            p.LastName,
+            p.Email,
+            p.PhoneNumber,
+            p.CreatedAt,
+            p.UpdatedAt
+        );
+
+    public static ParticipantDto ToParticipantDto(ParticipantEntity entity) =>
+        new(
+            entity.Id, 
+            entity.FirstName, 
+            entity.LastName, 
+            entity.Email, 
+            entity.PhoneNumber, 
+            entity.CreatedAt, 
+            entity.UpdatedAt
+        );
+}
