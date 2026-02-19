159167302fcb5556faf5d80355321fc5899d9957
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 15:26:34 +0100

    add UpdateTeacherDto

---
+++ CoursesManager.Application/Dtos/Teachers/UpdateTeacherDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoursesManager.Application.Dtos.Teachers;
+
+public class UpdateTeacherDto
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [MinLength(1)]
+    [MaxLength(50)]
+    public string Expertise { get; set; } = null!;
+}
