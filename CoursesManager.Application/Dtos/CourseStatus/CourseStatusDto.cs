4d0d6b6abe7f12073b28760be91e7c0cbb7518c0
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-19 16:51:40 +0100

    add CourseStatusDto for Application

---
+++ CoursesManager.Application/Dtos/CourseStatus/CourseStatusDto.cs
@@ -0,0 +1,6 @@
+﻿namespace CoursesManager.Application.Dtos.CourseStatus;
+
+public record CourseStatusDto(
+    int Id,
+    string StatusType
+);
---
+++ CoursesManager.Application/Dtos/CourseStatus/UpdateCourseStatusDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoursesManager.Application.Dtos.CourseStatus;
+
+public class UpdateCourseStatusDto
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(50)]
+    public string StatusType { get; set; } = null!;
+}
