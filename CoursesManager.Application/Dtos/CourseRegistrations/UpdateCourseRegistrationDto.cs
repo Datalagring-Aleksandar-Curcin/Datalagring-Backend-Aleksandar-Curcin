01b027ffe552bb1c3c1a3f1e96edad1715f70972
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-20 13:15:46 +0100

    add Coursesession & participant ID

--- CoursesManager.Application/Dtos/CourseRegistrations/UpdateCourseRegistrationDto.cs
+++ CoursesManager.Application/Dtos/CourseRegistrations/UpdateCourseRegistrationDto.cs
@@ -5,5 +5,11 @@ namespace CoursesManager.Application.Dtos.CourseRegistrations;
 public class UpdateCourseRegistrationDto
 {
     [Required]
+    public int CourseSessionId { get; set; }
+
+    [Required]
+    public int ParticipantId { get; set; }
+
+    [Required]
     public int CourseStatusId { get; set; }
 }
