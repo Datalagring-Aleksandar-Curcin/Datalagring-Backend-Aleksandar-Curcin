cfff452db143d16296cbd3f0a6085fdb12aa690d
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-20 14:21:28 +0100

    add migration

--- CoursesManager.Infrastructure/Persistence/Migrations/20260220131925_AddUniqueIndex_CourseRegistration_Participant_Session.Designer.cs
+++ CoursesManager.Infrastructure/Persistence/Migrations/20260220131925_AddUniqueIndex_CourseRegistration_Participant_Session.Designer.cs
@@ -4,6 +4,7 @@ using CoursesManager.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoursesManager.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260220131925_AddUniqueIndex_CourseRegistration_Participant_Session")]
+    partial class AddUniqueIndex_CourseRegistration_Participant_Session
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CoursesManager.Infrastructure/Persistence/Migrations/20260220131925_AddUniqueIndex_CourseRegistration_Participant_Session.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoursesManager.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndex_CourseRegistration_Participant_Session : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CourseRegistrations_ParticipantId",
+                table: "CourseRegistrations");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_CourseRegistration_Participant_Session",
+                table: "CourseRegistrations",
+                columns: new[] { "ParticipantId", "CourseSessionId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ_CourseRegistration_Participant_Session",
+                table: "CourseRegistrations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseRegistrations_ParticipantId",
+                table: "CourseRegistrations",
+                column: "ParticipantId");
+        }
+    }
+}
