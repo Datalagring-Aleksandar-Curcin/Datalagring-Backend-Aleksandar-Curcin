a086b5b9cccef3663dc08bca3608ed12c8493597
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-20 13:13:49 +0100

    remove comment

--- CoursesManager.Application/Dtos/CourseRegistrations/CreateCourseRegistrationDto.cs
+++ CoursesManager.Application/Dtos/CourseRegistrations/CreateCourseRegistrationDto.cs
@@ -13,7 +13,5 @@ public class CreateCourseRegistrationDto
     [Required]
     public int CourseStatusId { get; set; }
 
-    // Valfritt: om du vill att klienten kan skicka datum.
-    // Annars sätter vi RegistrationDate = UtcNow i service.
     public DateTime? RegistrationDate { get; set; }
 }
