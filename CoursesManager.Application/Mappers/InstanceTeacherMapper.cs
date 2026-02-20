b4402300e8955aa6e1c5ae381e97bc970b49e93e
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-02-20 10:01:48 +0100

    add InstanceTeacherMapper to Application

---
+++ CoursesManager.Application/Mappers/InstanceTeacherMapper.cs
@@ -0,0 +1,20 @@
+﻿using CoursesManager.Application.Dtos.InstanceTeachers;
+using CoursesManager.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace CoursesManager.Application.Mappers;
+
+public static class InstanceTeacherMapper
+{
+    public static Expression<Func<InstanceTeacherEntity, InstanceTeacherDto>> ToInstanceTeacherDtoExpr =>
+        it => new InstanceTeacherDto(
+            it.CourseSessionId,
+            it.TeacherId
+        );
+
+    public static InstanceTeacherDto ToInstanceTeacherDto(InstanceTeacherEntity entity) =>
+        new(
+            entity.CourseSessionId, 
+            entity.TeacherId
+        );
+}
